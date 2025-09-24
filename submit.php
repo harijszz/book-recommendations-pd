a1f1471d08dc06e7e818cc5ed31d1d113a2e73cb
unknown <you@example.com>
2025-09-24 12:11:54 +0300

    PHP gandriz 6. uzdevums

--- submit.php
+++ submit.php
@@ -1,5 +1,29 @@
 <?php
+$servername = "localhost";
+$username = "book_review_user_24092025";
+$password = "password";
+$dbname = "book_review_24092025";
+
 $fname = $lname = $levels = $name = $comment = "";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("INSERT INTO book_review (full_name, book_title, review_text, rating)
+  VALUES (:fname :lname, :name, :comment, :levels)");
+  $stmt->bindParam(':fname', $fname);
+  $stmt->bindParam(':lname', $lastname);
+  $stmt->bindParam(':levels', $levels);
+  $stmt->bindParam(':name', $name);
+  $stmt->bindParam(':comment', $comment);
+  $stmt->execute();
+  echo "New record created successfully";
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $fname = htmlspecialchars($_POST["fname"]);
@@ -8,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST["name"]);
   $comment = htmlspecialchars($_POST["comment"]);
 }
+$conn = null;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,15 +42,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Document</title>
 </head>
 <body>
-Vārds: <input type="text" name="fname" value="<?php echo $fname;?>">
+<br>Vārds: <input type="text" name="fname" value="<?php echo $fname;?>"><br>
 
-Uzvārds: <input type="text" name="lname" value="<?php echo $lname;?>">
+<br>Uzvārds: <input type="text" name="lname" value="<?php echo $lname;?>"><br>
 
-Līmenis: <select name="levels" value="<?php echo $levels;?>">
+<br>Līmenis: <select name="levels" value="<?php echo $levels;?>"><br>
 
-Nosaukums: <input type="text" name="name" value="<?php echo $name;?>">
+<br>Nosaukums: <input type="text" name="name" value="<?php echo $name;?>"><br>
 
-Apraksts: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+<br>Apraksts: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea><br>
 <a href="/index.html">Atpakaļ uz sākumlapu</a>
     
 </body>
